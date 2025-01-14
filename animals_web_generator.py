8916f962df7decd82d609e52ddeaf3a9fa6700c5
Reshma John <reshma.chackola@gmail.com>
2025-01-14 22:00:14 +0100

    Added comments for functions

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,21 +2,41 @@ import json
 
 
 def load_html_data(file_path):
+    """
+    This fn reads the "animal_template.html" file .
+    :param file_path:
+    :return: file content
+    """
     with open(file_path, "r") as handle:
         return handle.read()
 
 
 def load_data(file_path):
+    """
+    This fn reads the "animal_data.json" file .
+    :param file_path:
+    :return: file content
+    """
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
 def write_to_file(data):
+    """
+    This fn writes into "animals.html" file .
+    :param data:
+    """
     with open("animals.html", "w") as handle:
-        return handle.write(data)
+        handle.write(data)
 
 
 def serialize_animal(animal_obj):
+    """
+    This fn iterates through the animal_obj and extract the required data and formulates,
+    serialized data ready to be written into "animals.html" file .
+    :param animal_obj:
+    :return: file content
+    """
     animal_data_string = ""
     name = animal_obj.get("name")
     characteristics = animal_obj.get("characteristics", {})
@@ -24,9 +44,8 @@ def serialize_animal(animal_obj):
     location = animal_obj.get("locations", [])[0]
     common_name = characteristics.get("common_name")
     lifespan = characteristics.get("lifespan")
-    skin_type = characteristics.get("skin_type")
-
     animal_type = characteristics.get("type")
+
     animal_data_string += "<li class='cards__item'>\n"
     animal_data_string += "<div class ='card__title' > "f"{name}</div >\n"
     animal_data_string += "<p class='card__text'>\n"
@@ -48,20 +67,26 @@ def serialize_animal(animal_obj):
     animal_data_string += '</li>\n'
     return animal_data_string
 
+
 def generate_html_data(animals_data):
+    """
+    This fn act as a wrapper fn for the function  serialize_animal.
+    :param animals_data:
+    """
     return  "".join(serialize_animal(animal) for animal in animals_data)
 
+
 def main() :
+    """
+    This is the main function through wich the sub functions get invoked .
+    """
+
     animal_data_html = load_html_data("animals_template.html")
     animals_data = load_data("animals_data.json")
     animal_data_string = generate_html_data(animals_data)
     new_data = animal_data_html.replace("__REPLACE_ANIMALS_INFO__", animal_data_string)
-    print(new_data)
     write_to_file(new_data)
-
 
 
 if __name__ == "__main__":
     main()
-
-
